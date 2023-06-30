0e30bbc4c31a58802f8b0b70278d2f4e8fb52158
dteh6 <dteh69@gmail.com>
2023-06-30 19:22:37 +0800

    yes

--- src/PROG/Department.java
+++ src/PROG/Department.java
@@ -3,42 +3,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package PROG;
-
 /**
  *
- * @author dteh6
+ * @author dteh69
  */
-public class Department {
-    private String sDeptName;
-    private Employee[] eEmployees = new Employee[10];
-    
-    Department(String sName, Employee[] employees) {
-        this.sDeptName = sName;
-        this.eEmployees = employees;
-    }
-    
-    Department() {
-        this.sDeptName = "";
-        this.eEmployees = null;
-    }
-    
-    public void setDeptName(String sDeptName) {
-        this.sDeptName = sDeptName;
-    }
-    
-    public String getDeptName() {
-        return sDeptName;
-    }
-    
-    public void setDeptEmployees(Employee[] employees) {
-        this.eEmployees = employees;
-    }
-    
-    public Employee[] getDeptEmployees() {
-        return eEmployees;
-    }
-    
-    //number of employees in 1 dept
-    //total sal of dept
-    //avg sal of dept
+public class Department extends Assignment {
+	private String sDeptName;
+	private Employee[] eEmployees = new Employee[10];
+
+	Department(String sName, Employee[] employees) {
+		this.sDeptName = sName;
+		this.eEmployees = employees;
+	}
+
+	Department() {
+		this.sDeptName = "";
+		this.eEmployees = null;
+	}
+
+	public void setDeptName(String sDeptName) {
+		this.sDeptName = sDeptName;
+	}
+
+	public String getDeptName() {
+		return sDeptName;
+	}
+
+	public void setDeptEmployees(Employee[] employees) {
+		this.eEmployees = employees;
+	}
+
+	public void addDeptEmployee(Employee employee) {
+		this.eEmployees[eEmployees.length] = employee;
+	}
+
+	public Employee[] getDeptEmployees() {
+		return eEmployees;
+	}
+
+	// number of employees in 1 dept
+	// total sal of dept
+	// avg sal of dept
 }
--- src/PROG/Employee.java
+++ src/PROG/Employee.java
@@ -7,97 +7,115 @@ package PROG;
 
 /**
  *
- * @author Admin
+ * @author dteh69
  */
-class Employee extends Assignment{
+class Employee extends Assignment {
 	// declare student properties
-    	private int iID;
+	private int iID;
 	private String sName;
-        private double dSalary;
-        private String sDepartment;
+	private double dSalary;
+	private String sDepartment;
 
 	// initialise student property in constructor
 	Employee(int iID, String sName, double dSalary, String sDepartment) {
-            this.iID = iID;
-            this.sName = sName;
-            this.dSalary = dSalary;
-            this.sDepartment = sDepartment;
+		this.iID = iID;
+		this.sName = sName;
+		this.dSalary = dSalary;
+		this.sDepartment = sDepartment;
 	}
-        //function overloading for empty student object
-        Employee() {
-            this.iID = 0;
-            this.sName = "";
-            this.dSalary = 0;
-            this.sDepartment = "";
+
+	// function overloading for empty student object
+	Employee() {
+		this.iID = 0;
+		this.sName = "";
+		this.dSalary = 0;
+		this.sDepartment = "";
 	}
-        
-        public void setID(int iID) {
-            //this method sets the mobile of an instance of the student object
-            this.iID = iID;
-        }
-        
-        public int getID() {
-            //this method returns the mobile of an instance of the student object
-            return iID;
-        }
-        
-        public void setName(String name) {
-            //this method sets the name of an instance of the student object
-            this.sName = name.trim();
-        }
-        
-        public String getName() {
-            //this method returns the name of an instance of the student object
-            return sName;
-        }
-        
-        public void setSalary(double dSalary) {
-            //this method sets the ID of an instance of the student object
-            this.dSalary = dSalary;
-        }
-        public double getSalary() {
-            //this method returns the ID of an instance of the student object
-            return dSalary;
-        }
-        
-        public void setDepartment(String sDepartment) {
-            this.sDepartment = sDepartment;
-        }
-        
-        public String getDepartment() {
-            return sDepartment;
-        }
-        
+
+	public void setID(int iID) {
+		// this method sets the mobile of an instance of the student object
+		this.iID = iID;
+	}
+
+	public int getID() {
+		// this method returns the mobile of an instance of the student object
+		return iID;
+	}
+
+	public void setName(String name) {
+		// this method sets the name of an instance of the student object
+		this.sName = name.trim();
+	}
+
+	public String getName() {
+		// this method returns the name of an instance of the student object
+		return sName;
+	}
+
+	public void setSalary(double dSalary) {
+		// this method sets the ID of an instance of the student object
+		this.dSalary = dSalary;
+	}
+
+	public double getSalary() {
+		// this method returns the ID of an instance of the student object
+		return dSalary;
+	}
+
+	public void setDepartment(String sDepartment) {
+		this.sDepartment = sDepartment;
+	}
+
+	public String getDepartment() {
+		return sDepartment;
+	}
+
 	public String toString(int iIDLength, int iNameLength, int iSalaryLength, int iDepartmentLength) {
-            //this method adjusts the length of name, gender, ID and mobile and assigns it to a string
-            return adjustLength(Integer.toString(iID), iIDLength)+"     "+adjustLength(sName, iNameLength)+"     "+adjustLength(Double.toString(dSalary), iSalaryLength)+"     "+adjustLength(sDepartment, iDepartmentLength);
-        }
-        
-        public String padSpace(String sString, int ilength){
-            //add spaces behind strings so all lengths are equal
-            //returns the string with the  whitespace-padded string
-            while(sString.length()!=ilength)
-                {
-                    sString=sString+" ";
-                }
-            return sString;
-        }
-        
-        public String toFileString() {
-            //used for formatting the file when writing back into the file
-            //returns a string that is used when writing to the file
-            return Integer.toString(iID)+">>"+sName+">>"+Double.toString(dSalary)+">>"+sDepartment+">>";
-        }
-        
-        public String adjustLength(String sString, int iLength) {
-            //this method adjusts the length of the string that is passed in
-            //returns a string that has the same length as the other strings
-            if(sString.length()<=iLength)
-                return sString=padSpace(sString, iLength);
-            //if the string's length is more than iLength, it will truncate and return the string
-            else 
-            {
-                return sString=sString.substring(0, iLength-3)+"...";
-            }
-        }
-    }
+		// this method adjusts the length of ID, name, salary, department and assigns it
+		// to a string
+		return adjustLength(Integer.toString(iID), iIDLength) + "     " + adjustLength(sName, iNameLength) + "    $"
+				+ adjustLength(Double.toString(dSalary), iSalaryLength) + "     "
+				+ adjustLength(sDepartment, iDepartmentLength);
+	}
+
+	public String padSpace(String sString, int ilength) {
+		// add spaces behind strings so all lengths are equal
+		// returns the string with the whitespace-padded string
+		while (sString.length() != ilength) {
+			sString = sString + " ";
+		}
+		return sString;
+	}
+
+	public String toFileString() {
+		// used for formatting the file when writing back into the file
+		// returns a string that is used when writing to the file
+		return Integer.toString(iID) + ">>" + sName + ">>" + Double.toString(dSalary) + ">>" + sDepartment + ">>";
+	}
+
+	public String adjustLength(String sString, int iLength) {
+		// this method adjusts the length of the string that is passed in
+		// returns a string that has the same length as the other strings
+		if (sString.length() <= iLength)
+			return sString = padSpace(sString, iLength);
+		// if the string's length is more than iLength, it will truncate and return the
+		// string
+		else {
+			return sString = sString.substring(0, iLength - 3) + "...";
+		}
+	}
+
+	public void sortByID() {
+		// sort by moving smaller ID up the array
+		for (int i = 0; i < iNumEmployees; i++) {
+			// Checking the condition for two elements of the array
+			if (employees[i].getID() > employees[i + 1].getID()) {
+				// Swapping the elements.
+				Employee temp = employees[i];
+				employees[i] = employees[i + 1];
+				employees[i + 1] = temp;
+				i = -1;// make loop go forever until its done
+			}
+		}
+	}
+}
