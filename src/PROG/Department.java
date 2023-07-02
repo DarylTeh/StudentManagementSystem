73826415b1d174876e0758c9069e6fbbbea3e1ca
dteh6 <dteh69@gmail.com>
2023-07-02 12:12:01 +0800

    Fixed most bugs and error messages

--- src/PROG/Department.java
+++ src/PROG/Department.java
@@ -3,45 +3,73 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package PROG;
+
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dteh69
  */
 public class Department extends Assignment {
-	private String sDeptName;
-	private Employee[] eEmployees = new Employee[10];
 
-	Department(String sName, Employee[] employees) {
-		this.sDeptName = sName;
-		this.eEmployees = employees;
-	}
+    private String sDeptName;
+    private Employee[] eEmployees = new Employee[10];
 
-	Department() {
-		this.sDeptName = "";
-		this.eEmployees = null;
-	}
+    Department(String sName, Employee[] employees) {
+        this.sDeptName = sName;
+        this.eEmployees = employees;
+    }
 
-	public void setDeptName(String sDeptName) {
-		this.sDeptName = sDeptName;
-	}
+    Department() {
+        this.sDeptName = null;
+        this.eEmployees = new Employee[10];
+    }
 
-	public String getDeptName() {
-		return sDeptName;
-	}
+    public void setDeptName(String sDeptName) {
+        this.sDeptName = sDeptName;
+    }
 
-	public void setDeptEmployees(Employee[] employees) {
-		this.eEmployees = employees;
-	}
+    public String getDeptName() {
+        return sDeptName;
+    }
 
-	public void addDeptEmployee(Employee employee) {
-		this.eEmployees[eEmployees.length] = employee;
-	}
+    public void addDeptEmployee(int iID, String sName, double dSalary, String sDeptName) {
+        int count = 0;
+        if (this.getNumEmployees() == 10) {
+            errorMsg("You cannot have more than 10 employees in 1 Department");
+        } else {
+            if (eEmployees[0] == null) {
+                this.eEmployees[0] = new Employee(iID, sName, dSalary, sDeptName);
+            } else {
+                for (Employee emp : eEmployees) {
+                    if (emp != null) {
+                        count++;
+                    }
+                }
+                this.eEmployees[count] = new Employee(iID, sName, dSalary, sDeptName);
+            }
+        }
+    }
 
-	public Employee[] getDeptEmployees() {
-		return eEmployees;
-	}
+    public String toFileString() {
+        // used for formatting the file when writing back into the file
+        // returns a string that is used when writing to the file
+        return (this.sDeptName + ">>");
+    }
 
-	// number of employees in 1 dept
-	// total sal of dept
-	// avg sal of dept
+    public Employee[] getDeptEmployees() {
+        return eEmployees;
+    }
+
+    // number of employees in 1 dept
+    public int getNumEmployees() {
+        int count = 0;
+        for (Employee emp : eEmployees) {
+            if (emp != null) {
+                if(emp.getName() != null)
+                    count++;
+            }
+        }
+        return count;
+    }
 }
